4121cff9a9498691045725d385a572f3019c1780
Justyna Rachowicz <mrowa44@gmail.com>
2017-04-03 18:34:11 +0200

    Refactor, enhance package.json, add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'airbnb-base',
+  plugins: [],
+  rules: {
+    'global-require': 0,
+    'import/no-dynamic-require': 0,
+  },
+};
--- index.js
+++ index.js
@@ -4,28 +4,27 @@ const ora = require('ora');
 
 const CONFIG_NAME = 'yolo.json';
 
-const spinner = ora('Preparing...').start();
+module.exports = function run() {
+  const spinner = ora('Preparing...').start();
+  let config;
 
-let config;
-
-findUp(CONFIG_NAME)
-  .then((path) => {
-    config = require(path);
-    spinner.color = 'red';
-    spinner.text = 'Dropping database...';
-    return exec(config.drop);
-  })
-  .then(() => {
-    spinner.color = 'yellow';
-    spinner.text = 'Building database...';
-    return exec(config.build);
-  })
-  .then(() => {
-    spinner.color = 'green';
-    spinner.text = 'Migrating database...';
-    return exec(config.post);
-  })
-  .then(() => spinner.succeed('Successfully dropped and created database!'))
-  .catch((error) => {
-    spinner.fail(error.message);
-  });
+  findUp(CONFIG_NAME)
+    .then((path) => {
+      config = require(path);
+      spinner.color = 'red';
+      spinner.text = 'Dropping database...';
+      return exec(config.drop);
+    })
+    .then(() => {
+      spinner.color = 'yellow';
+      spinner.text = 'Building database...';
+      return exec(config.build);
+    })
+    .then(() => {
+      spinner.color = 'green';
+      spinner.text = 'Migrating database...';
+      return exec(config.post);
+    })
+    .then(() => spinner.succeed('Successfully dropped and created database!'))
+    .catch(error => spinner.fail(error.message));
+};
