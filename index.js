c715f696779990c3154e4fe3b1baea9c3e9a5244
Justyna Rachowicz <mrowa44@gmail.com>
2017-04-10 17:38:55 +0200

    Add init option

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 'use strict';
 
-const exec = require('child-process-promise').exec;
+const fs = require('fs');
+const fsp = require('fs-promise');
 const findUp = require('find-up');
 const ora = require('ora');
+const exec = require('child-process-promise').exec;
 
 const CONFIG_NAME = 'yolo.json';
 
-module.exports = function run() {
+function initialize() {
+  const spinner = ora().start();
+  const configPath = `./${CONFIG_NAME}`;
+  const examplePath = `${__dirname}/${CONFIG_NAME}.example`;
+
+  spinner.text = 'Checking if file exists...';
+  if (fs.existsSync(configPath)) {
+    spinner.fail(`${CONFIG_NAME} already exists!`);
+    return null;
+  }
+
+  spinner.text = `Creating ${CONFIG_NAME} file...`;
+  return fsp.copy(examplePath, configPath)
+    .then(() => spinner.succeed(`Created ${CONFIG_NAME}`))
+    .catch(error => spinner.fail(error.message));
+}
+
+function yolo() {
   const spinner = ora('Preparing...').start();
   let config;
 
-  findUp(CONFIG_NAME)
+  return findUp(CONFIG_NAME)
     .then((path) => {
       config = require(path);
       spinner.color = 'red';
@@ -29,4 +48,12 @@ module.exports = function run() {
     })
     .then(() => spinner.succeed('Successfully dropped and created database!'))
     .catch(error => spinner.fail(error.message));
+}
+
+module.exports = () => {
+  if (process.argv.includes('init')) {
+    return initialize();
+  }
+
+  return yolo();
 };
